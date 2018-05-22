496ded66db15874778d6b4e7c5c5c0793a3d1f46
Kamil Chmielewski <kamil.chmielewski@outlook.com>
2018-05-22 18:08:34 +0200

    Dodaj pliki projektów.

---
+++ GoogleSpeechForWord/HandlerAddIn.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+
+namespace GoogleSpeechForWord
+{
+    public partial class HandlerAddIn
+    {
+        private InterfaceForm interfaceForm;
+
+        private void HandlerAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void HandlerAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Kod wygenerowany przez program VSTO
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(HandlerAddIn_Startup);
+            this.Shutdown += new System.EventHandler(HandlerAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ GoogleSpeechForWord/SpeechRecognition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Speech.V1;
+
+namespace GoogleSpeechForWord
+{
+    class SpeechRecognition
+    {
+    }
+}
